1f4b7bed02991d8866d32a9d8c2cd7b350f44ba9
KASHISH <kashu80525@gmail.com>
2023-11-04 19:01:12 +0530

    adding chnages

--- block-BNaabs/sample/index.js
+++ block-BNaabs/sample/index.js
@@ -11,10 +11,13 @@ app.use(express.urlencoded({ extended: false }));
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
+
 app.get("/new", (req, res) => {
   res.sendFile(__dirname + "/new.html");
 });
-
+app.post("/new", (req, res) => {
+  res.json(req.body);
+});
 app.get("/users/:username", (req, res) => {
   var username = req.params.username;
   res.send(username);
